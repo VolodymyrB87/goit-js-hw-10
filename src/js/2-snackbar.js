15df3ae8c881c5b79d22d41d908178ae133aab67
VolodymyrB <volodymyrbedryk@gmail.com>
2026-03-15 19:30:31 +0100

    hm10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,32 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+const form = document.querySelector('.form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  const delay = formData.get('delay');
+  const state = formData.get('state');
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+  promise
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'center',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'center',
+      });
+    });
+});
