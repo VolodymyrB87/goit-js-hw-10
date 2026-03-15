576689d63a9f763b8abae24c91e5523f8dc15592
VolodymyrB87 <volodymyrbedryk@gmail.com>
2026-03-15 18:35:59 +0000

    Deploying to gh-pages from @ VolodymyrB87/goit-js-hw-10@15df3ae8c881c5b79d22d41d908178ae133aab67 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as o}from"./assets/vendor-BbbuE1sJ.js";const i=document.querySelector(".form");i.addEventListener("submit",r=>{r.preventDefault();const s=new FormData(i),t=s.get("delay"),m=s.get("state");new Promise((e,n)=>{setTimeout(()=>{m==="fulfilled"?e(t):n(t)},t)}).then(e=>{o.success({message:`✅ Fulfilled promise in ${e}ms`,position:"center"})}).catch(e=>{o.error({message:`❌ Rejected promise in ${e}ms`,position:"center"})})});
+//# sourceMappingURL=2-snackbar.js.map
